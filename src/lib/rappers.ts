89641b0fb04feb6b46ff60d87f0399eaf3d6b966
holyrocking <zggshsgsgsh@gmail.com>
2026-07-07 20:24:48 +0300

    (feat): add rapper type and basic rapper database array

--- src/lib/rappers.ts
+++ src/lib/rappers.ts
@@ -1,3 +1,10 @@
+import fakemink from "../assets/rappers/fakemink.jpg"
+import yeat from "../assets/rappers/yeat.jpg"
+import nine from "../assets/rappers/ninevicious.png"
+import bleood from "../assets/rappers/bleood.jpg"
+import carti from "../assets/rappers/carti.jpg"
+
+type rarity = 'common'|'rare'|'epic'|'arcane'
 
 type rapperStatus = "locked"|"unlocked"|"active"
 
@@ -6,9 +13,63 @@ export type rapper ={
 
     id:string,
     name:string,
+    about:string,
+    rapperImg:string,
     clickPower:number,
     leanPerSecond:number,
-    status:rapperStatus;
+   
+    rapperRarity:rarity
 
     
 }
+
+
+
+export const RAPPERS:rapper[] = [
+    {
+        id:"yeat",
+        name:"Yeat",
+        about:"I DON'T LIKE HIM NOW!!! MINION!",
+        rapperImg:yeat,
+        clickPower:1,
+        leanPerSecond:0,
+        rapperRarity:"common"
+    },
+    {
+        id:"ninevicious",
+        name:"Nine Vicious",
+        about:"What I'm 'posed to do when these racks blue?",
+        rapperImg:nine,
+        clickPower:3,
+        leanPerSecond:1,
+        rapperRarity:"rare"
+        
+    },
+    {
+        id:"fakemink",
+        name:"fakemink",
+        about:"My Sprite like Easter Pink",
+        rapperImg:fakemink,
+        clickPower:3,
+        leanPerSecond:1,
+        rapperRarity:"rare"
+    },
+    {
+        id:"bleood",
+        name:"bleood",
+        about:"King RASCAL",
+        rapperImg:bleood,
+        clickPower:25,
+        leanPerSecond:10,
+        rapperRarity:"epic"
+    },
+    {
+        id:"carti",
+        name:"Playboi Carti",
+        about:"KING VAMP YVL",
+        rapperImg:carti,
+        clickPower:100,
+        leanPerSecond:35,
+        rapperRarity:"epic"
+    }
+]
