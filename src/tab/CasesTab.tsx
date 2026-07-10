f3a19f5cc9fda2b336c10020780a128cedab2583
holyrocking <zggshsgsgsh@gmail.com>
2026-07-10 17:59:35 +0300

    (feat):add cases animations and refactor styles file

--- src/tab/CasesTab.tsx
+++ src/tab/CasesTab.tsx
@@ -2,7 +2,14 @@
 
 const CasesTab = () => {
   return (
-    <div>
+    <div className="relative overflow-hidden  w-[1000px]  h-auto mt-[100px] rounded-2xl mb-[100px]
+        bg-gradient-to-b from-zinc-900 to-[#141414]     mx-auto 
+       shadow-[0_0_40px_rgba(255,255,255,0.06)] p-6">
+
+        <div>
+
+          
+        </div>
       
     </div>
   )
